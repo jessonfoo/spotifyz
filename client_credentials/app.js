db42da7220d4e12d1ee131767e3cb9e7b17133bf
jessonfoo <jessonfoo@gmail.com>
2016-07-13 22:04:41 -0700

    testing spotify web endpoints w/ node

---
+++ client_credentials/app.js
@@ -0,0 +1,33 @@
+var request = require('request'); // "Request" library
+var client_id = '583cbfe0843a4392ae8af9bbe87acf2b'; // client id
+var client_secret = 'ef3f8e246edf4a16bea4e1297c87f883'; // secret
+
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+
+    // use the access token to access the Spotify Web API
+    var token = body.access_token;
+    var options = {
+      url: 'https://api.spotify.com/v1/users/jessonfoo',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true
+    };
+    request.get(options, function(error, response, body) {
+      console.log(body);
+    });
+  }
+});
